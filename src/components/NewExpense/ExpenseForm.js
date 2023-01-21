38c15c6ad708aa12d25dd2c84faae64927ed1a4c
asdrubal ovalles <ovallesa@me.com>
2023-01-21 17:28:56 -0300

    updating Expense form

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,23 +1,38 @@
 import './ExpenseForm.css';
-
+import React, {useState} from  'react';
 
 function ExpenseForm() {
+    const [enteredTitle, setEnteredTitle] = useState('')
+    function titleChangeHandler(event){
+        setEnteredTitle(event.target.value)
+    }
+
+    const [enteredDate, setEnteredDate] = useState('')
+    function dateChangeHandler(event){
+        setEnteredDate(event.target.value)
+    }
+
+    const [enteredAmount, setEnteredAmount] = useState('')
+    function amountChangeHandler(event){
+        setEnteredAmount(event.target.value)
+    }
+
     return (
         <form>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type="text"></input>
+                    <input type="text" onChange={titleChangeHandler}></input>
 
                 </div>
                 <div className="new-expense__control">
                     <label>Date</label>
-                    <input type="date" min="2019-01-01" max="2022-12-31"></input>
+                    <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler}></input>
 
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
-                    <input type="number"></input>
+                    <input type="number" onChange={amountChangeHandler}></input>
 
                 </div>
 
